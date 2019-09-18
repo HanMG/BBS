0beae8fdd324543054b603f470c5c04c00cad006
Administrator <Administrator@DESKTOP-1JL2KJE>
2019-09-18 13:27:36 +0900

    join added

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -49,4 +49,22 @@ public class UserDAO {
 		}
 		return -2; // 데이터베이스 오류
 	}
+
+	public int join(User user) {
+		String sql = "INSERT INTO user VALUES(?,?,?,?,?)";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getUserGender());
+			pstmt.setString(5, user.getUserEmail());
+			return pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return -2; // 데이터베이스 오류
+	}
 }
